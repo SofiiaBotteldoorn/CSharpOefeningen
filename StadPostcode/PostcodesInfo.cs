ffd7b70e10031c51919ad172347c3daa6aea7697
Sofiia <sofiia.botteldoorn@vdabcampus.be>
2025-04-10 17:02:34 +0200

    Oefening met bestand

---
+++ StadPostcode/PostcodesInfo.cs
@@ -0,0 +1,22 @@
+﻿namespace StadPostcode
+{
+    public class PostcodesInfo
+    {
+        public int Postcode(string inputStad)
+        {
+            string bestand = Path.Combine(AppContext.BaseDirectory, "postcodes.txt");
+            using (StreamReader lezer = new StreamReader(bestand))
+            {
+                string? regel;
+                while ((regel = lezer.ReadLine()) != null)
+                {
+                    int streepje = regel.IndexOf('-');
+                    string stad = regel.Substring(0, streepje - 1);
+                    if (stad.ToLower() == inputStad.Trim().ToLower())
+                        return int.Parse(regel.Substring(streepje + 2));
+                }
+            }
+            throw new Exception($"Stad {inputStad} is niet gevonden.");
+        }
+    }
+}
---
+++ StadPostcode/Program.cs
@@ -0,0 +1,39 @@
+﻿using StadPostcode;
+
+string inputStad = LeesStad();
+while (inputStad != "stop")
+{
+    ToonPostcode(inputStad);
+    inputStad = LeesStad();
+}
+
+string LeesStad()
+{
+    while (true)
+    {
+        Console.WriteLine($"Stad (stop om te stoppen): ");
+        string? inputStad = Console.ReadLine();
+        if (string.IsNullOrWhiteSpace(inputStad) || inputStad.Any(char.IsDigit))
+        {
+            Console.WriteLine("Onmogelijk. Stad naam mag niet leegzijn en mag geen cijfers bevatten");
+            Console.WriteLine();
+        }
+        else
+        {
+            return inputStad;
+        }
+    }
+}
+void ToonPostcode(string inputStad)
+{
+    try
+    {
+        PostcodesInfo info = new PostcodesInfo();
+        Console.WriteLine($"De postcode van {inputStad} is {info.Postcode(inputStad)}");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+    Console.WriteLine();
+}
